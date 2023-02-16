9a02f9a909272edadc44f4db4d19fc7ba61bb288
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-02-16 16:10:24 +0100

    modification du mode de remplissage + début du CSS

--- src/components/Livre.vue
+++ src/components/Livre.vue
@@ -5,12 +5,24 @@
 
 <template>
   <li>
-    {{ livre.pourAfficher() }}
-    <button @click="$emit('add1', indexl)">^</button>
-    <button @click="$emit('del1', indexl)">v</button>
-    <button @click="$emit('deletel', indexl)">Supprimer</button>
+    <p id="titr">{{livre.titre}}</p>
+    <p id="qte">{{livre.qtestock}}</p>
+    <p id="prix">{{livre.prix}}</p>
+    <button id="sup" @click="$emit('add1', indexl)">^</button>
+    <button id="inf" @click="$emit('del1', indexl)">v</button>
+    <button id="del" @click="$emit('deletel', indexl)">Supprimer</button>
   </li>
 </template>
 
-<style>
+<style scoped>
+li {
+  display:inline-block; background: #555555; vertical-align: top;
+  width: 150px; height:200px; border-radius: 10px; margin:5px 10px 5px 10px;
+}
+
+p#titr {
+  color: #ffffff; text-align: center;
+  text-transform: uppercase; width: 150px; font-size: 2rem;
+  
+}
 </style>
--- src/components/ModeRempli.vue
+++ src/components/ModeRempli.vue
@@ -1,24 +1,84 @@
 <script setup>
-    import Remplisseur from "./Remplisseur.vue";
+    import { onMounted } from "vue";
+    import { ref } from "vue";
 
-    function handlerAdd(titre,qtestock,prix) {
+    const titre = ref("");
+    const qtestock = ref("");
+    const prix = ref("");
+    let listeT = [];
+    
+    function handlerSubmit() {
         const head=new Headers()
         head.append("Content-Type", "application/json")
         const fetchOptions={method: "POST",
             headers: head,
-            body: JSON.stringify({ titre : titre ,
-                                   qtestock : qtestock ,
-                                   prix : prix })}
+            body: JSON.stringify({ titre : titre.value ,
+                                   qtestock : qtestock.value ,
+                                   prix : prix.value })}
         fetch("https://webmmi.iut-tlse3.fr/~pecatte/librairies/public/5/livres",fetchOptions)
+            .then((response) => {
+                titre.value = "";
+                qtestock.value = "";
+                prix.value = "";
+            })
             .catch((error) =>
                 console.log(error)
             )
     }
+
+    function actionTitre() {
+        const url="https://webmmi.iut-tlse3.fr/~pecatte/librairies/public/5/livres?search="+titre.value;
+        const fetchOptions={};
+        fetch(url,fetchOptions)
+            .then((response) =>{
+                return response.json()
+                }
+            )
+            .then((dataJSON)=>{
+                listeT.splice(0, listeT.length);
+                dataJSON.forEach((v) =>
+                    listeT.push(v))
+                let titres=[]
+                dataJSON.forEach((v) =>
+                    titres.push(v.titre))
+                if (titres.indexOf(titre.value)!=-1){
+                    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+                }
+            })
+            .catch((error)=>{
+                console.log(error)
+                }
+            )
+    }
+
+    onMounted(() => {
+    actionTitre();
+    });
 </script>
 
 <template>
-    <Remplisseur @addl="handlerAdd"></Remplisseur>
+    <form @submit.prevent="handlerSubmit">
+        <input type="text" v-model="titre" placeholder="Nom du nouveau livre" @input="actionTitre"/>
+        <input type="number" min="1" v-model="qtestock" placeholder="Quantitée en stock"/>
+        <input type="number" min="0" step="0.01" v-model="prix" placeholder="Prix"/>
+        <input type="submit" value="Créer"/>
+    </form>
+    <div>
+        <p id="titr">{{titre}}</p>
+        <p id="qte">{{qtestock}}</p>
+        <p id="prix">{{prix}}</p>
+    </div>
 </template>
 
 <style scoped>
+div {
+  display:inline-block; background: #555555; vertical-align: top;
+  width: 150px; height:200px; border-radius: 10px; margin:5px 10px 5px 10px;
+}
+
+p#titr {
+  color: #ffffff; text-align: center;
+  text-transform: uppercase; width: 150px; font-size: 1rem;
+  
+}
 </style>
